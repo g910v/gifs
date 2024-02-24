349f24cc8b51d16d4514c929cc337c5cc2f28486
Виктория Гнитеева <gniteeva.v@mail.ru>
2024-02-24 16:04:58 +0300

    fix

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,2 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-// interface ImportMetaEnv {
-//   VITE_API_URL: string,
-//   VITE_WS_URL: string,
-// }
